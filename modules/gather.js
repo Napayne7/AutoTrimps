8bca31e9062810a6fffdb0fe61347f55c0594750
Napayne7 <41966473+Napayne7@users.noreply.github.com>
2020-01-26 16:51:16 +0100

    Update gather.js

--- modules/gather.js
+++ modules/gather.js
@@ -16,6 +16,8 @@ function manualLabor2() {
     var targetBreed = getPageSetting('GeneticistTimer');
     var trapperTrapUntilFull = game.global.challengeActive == "Trapper" && notFullPop;
     var hasTurkimp = game.talents.turkimp2.purchased || game.global.turkimpTimer > 0;
+	
+	console.log(getBreedTime(true));
 
     //FRESH GAME NO HELIUM CODE.
     if (game.global.world <=3 && game.global.totalHeliumEarned<=5000) {
